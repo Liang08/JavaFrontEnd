46af6d4b9a9a70e60cfa6733406e5e477cf9580c
liang <3417689373@qq.com>
2021-08-15 22:55:58 +0800

    login ui complete

--- app/src/main/java/com/java/xuhaotian/user/LoginActivity.java
+++ app/src/main/java/com/java/xuhaotian/user/LoginActivity.java
@@ -3,14 +3,26 @@ package com.java.xuhaotian.user;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.java.xuhaotian.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText etUser, etPassword;
+    private Button mBtnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initViews();
+    }
+    public void initViews(){
+        etUser = findViewById(R.id.et_username);
+        etPassword = findViewById(R.id.et_password);
+        mBtnLogin = findViewById(R.id.btn_login);
+
+
     }
 }
